2eeff872b64eec2e9c9b8df9b062285a54989fc2
Arnav Gupta <arnav.gupta@zomato.com>
2019-03-25 15:47:56 +0530

    add tests for color contrast util
    Signed-off-by: Arnav Gupta <arnav.gupta@zomato.com>

--- sushilib/src/main/java/com/zomato/sushilib/utils/color/ColorContrastUtils.kt
+++ sushilib/src/main/java/com/zomato/sushilib/utils/color/ColorContrastUtils.kt
@@ -12,9 +12,9 @@ class ColorContrastUtils {
          */
         @JvmStatic
         fun isDarkColor (color: Int): Boolean {
-            val r = Color.red(color)
-            val g = Color.green(color)
-            val b = Color.blue(color)
+            val r = color shr 16 and 0xFF // Color.red(color)
+            val g = color shr 8 and 0xFF // Color.green(color)
+            val b = color and 0xFF // Color.blue(color)
             val yiq = ((r*299)+(g*587)+(b*114))/1000
             return (yiq < 128)
         }
