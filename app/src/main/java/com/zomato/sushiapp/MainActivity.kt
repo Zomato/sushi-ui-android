38876e95c810df196e1b2cdc6413750799dcfdb2
Arnav Gupta <arnav.gupta@zomato.com>
2019-03-25 11:26:28 +0530

    initial sushi atoms done
    Signed-off-by: Arnav Gupta <arnav.gupta@zomato.com>

---
+++ app/src/main/java/com/zomato/sushiapp/MainActivity.kt
@@ -0,0 +1,81 @@
+package com.zomato.sushiapp
+
+import android.os.Bundle
+import android.support.design.widget.Snackbar
+import android.support.design.widget.NavigationView
+import android.support.v4.view.GravityCompat
+import android.support.v7.app.ActionBarDrawerToggle
+import android.support.v7.app.AppCompatActivity
+import android.view.Menu
+import android.view.MenuItem
+import com.zomato.sushiapp.fragments.ColorPaletteFragment
+import com.zomato.sushiapp.fragments.TextStylesFragment
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.app_bar_main.*
+
+class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        setSupportActionBar(toolbar)
+
+        fab.setOnClickListener { view ->
+            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                .setAction("Action", null).show()
+        }
+
+        val toggle = ActionBarDrawerToggle(
+            this,
+            drawer_layout,
+            toolbar,
+            R.string.navigation_drawer_open,
+            R.string.navigation_drawer_close
+        )
+        drawer_layout.addDrawerListener(toggle)
+        toggle.syncState()
+
+        nav_view.setNavigationItemSelectedListener(this)
+    }
+
+    override fun onBackPressed() {
+        if (drawer_layout.isDrawerOpen(GravityCompat.START)) {
+            drawer_layout.closeDrawer(GravityCompat.START)
+        } else {
+            super.onBackPressed()
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        menuInflater.inflate(R.menu.main, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        when (item.itemId) {
+            R.id.action_settings -> return true
+            else -> return super.onOptionsItemSelected(item)
+        }
+    }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        // Handle navigation view item clicks here.
+        val fragment = when (item.itemId) {
+            R.id.nav_text_styles -> TextStylesFragment()
+            R.id.nav_color_palette -> ColorPaletteFragment()
+            else -> TextStylesFragment()
+        }
+
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.container_main, fragment)
+            .commit()
+
+
+        drawer_layout.closeDrawer(GravityCompat.START)
+        return true
+    }
+}
---
+++ app/src/main/java/com/zomato/sushiapp/fragments/ColorPaletteFragment.kt
@@ -0,0 +1,27 @@
+package com.zomato.sushiapp.fragments
+
+
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+import com.zomato.sushiapp.R
+
+/**
+ * A simple [Fragment] subclass.
+ *
+ */
+class ColorPaletteFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_color_palette, container, false)
+    }
+
+
+}
