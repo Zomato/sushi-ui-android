780950f31dbe8a852f732d170a87d431c417de20
Arnav Gupta <arnav.gupta@zomato.com>
2019-03-26 13:03:51 +0530

    add ZTextView attributes for font weight and size
    Signed-off-by: Arnav Gupta <arnav.gupta@zomato.com>

--- sushilib/src/main/java/com/zomato/sushilib/atoms/ZTextView.kt
+++ sushilib/src/main/java/com/zomato/sushilib/atoms/ZTextView.kt
@@ -3,22 +3,35 @@ package com.zomato.sushilib.atoms
 import android.content.Context
 import android.util.AttributeSet
 import android.widget.TextView
+import com.zomato.sushilib.R
+import com.zomato.sushilib.utils.text.TextFormatUtils
 
 class ZTextView : TextView {
-    constructor(context: Context?) : super(context)
-    constructor(context: Context?, attrs: AttributeSet) : super(context, attrs)
-    constructor(context: Context?, attrs: AttributeSet, defStyleAttr: Int) : super(
-        context,
-        attrs,
-        defStyleAttr
-    ) {
-//        context?.theme?.obtainStyledAttributes(
-//            attrs,
-//            arrayOf(R.styleable.ZTextView_textSize).toIntArray(),
-//            defStyleAttr,
-//            R.style.SushiTheme
-//        )?.let {
-//            val textSize = it.getIn(R.styleable.ZTextView_textSize, 14f)
-//        }
+    constructor(context: Context?) : this(context, null)
+    constructor(context: Context?, attrs: AttributeSet?) :
+            this(context, attrs, 0)
+
+    constructor(context: Context?, attrs: AttributeSet?, defStyleAttr: Int) :
+            this(context, attrs, defStyleAttr, 0)
+
+    constructor(
+        context: Context?,
+        attrs: AttributeSet?, defStyleAttr: Int, defStyleRes: Int
+    ) : super(context, attrs, defStyleAttr, defStyleRes) {
+
+        context?.theme?.obtainStyledAttributes(
+            attrs,
+            R.styleable.ZTextView,
+            defStyleAttr,
+            R.style.SushiTheme
+        )?.let {
+            val zTextSize = it.getInt(R.styleable.ZTextView_zTextSize, 500)
+            textSize = TextFormatUtils.zTextSizeToAndroidSp(zTextSize)
+
+            val zFontWeight = it.getInt(R.styleable.ZTextView_zFontWeight, 500)
+            typeface = TextFormatUtils.zFontWeightToTypeface(context, zFontWeight)
+
+            it.recycle()
+        }
     }
 }
