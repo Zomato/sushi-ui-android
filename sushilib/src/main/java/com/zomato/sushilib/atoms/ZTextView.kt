3201d20bc875606cd1dd08e1f4dec0056d057e22
Arnav Gupta <arnav.gupta@zomato.com>
2019-03-26 12:18:14 +0530

    add more textviews for sample and start creating basic ztextview (#3)
    * start creating ZTextView
    
    Signed-off-by: Arnav Gupta <arnav.gupta@zomato.com>
    
    * reformat code and imports
    
    Signed-off-by: Arnav Gupta <arnav.gupta@zomato.com>

---
+++ sushilib/src/main/java/com/zomato/sushilib/atoms/ZTextView.kt
@@ -0,0 +1,24 @@
+package com.zomato.sushilib.atoms
+
+import android.content.Context
+import android.util.AttributeSet
+import android.widget.TextView
+
+class ZTextView : TextView {
+    constructor(context: Context?) : super(context)
+    constructor(context: Context?, attrs: AttributeSet) : super(context, attrs)
+    constructor(context: Context?, attrs: AttributeSet, defStyleAttr: Int) : super(
+        context,
+        attrs,
+        defStyleAttr
+    ) {
+//        context?.theme?.obtainStyledAttributes(
+//            attrs,
+//            arrayOf(R.styleable.ZTextView_textSize).toIntArray(),
+//            defStyleAttr,
+//            R.style.SushiTheme
+//        )?.let {
+//            val textSize = it.getIn(R.styleable.ZTextView_textSize, 14f)
+//        }
+    }
+}
