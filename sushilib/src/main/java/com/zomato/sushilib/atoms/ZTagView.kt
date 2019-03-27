b161759bc376e630abfef1f3fcf577e90f57280e
Prateek Sharma <sharma.prateek@zomato.com>
2019-03-27 20:41:38 +0530

    basic dimen intro

--- sushilib/src/main/java/com/zomato/sushilib/atoms/ZTagView.kt
+++ sushilib/src/main/java/com/zomato/sushilib/atoms/ZTagView.kt
@@ -61,7 +61,8 @@ class ZTagView : ZTextView, RoundedView {
             return when (type) {
                 1 -> R.style.SushiTheme_TagAppearance_RoundedTag
                 2 -> R.style.SushiTheme_TagAppearance_SmallRoundedTag
-                3 -> R.style.SushiTheme_TagAppearance_SemiBoldRoundedTag
+                3 -> R.style.SushiTheme_TagAppearance_HighlightRoundedTag
+                4-> R.style.SushiTheme_TagAppearance_SmallHighlightRoundedTag
                 else -> R.style.SushiTheme_TagAppearance_RoundedTag
             }
         }
