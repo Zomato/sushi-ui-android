e1049aad883c591846883d200a70226667ce0a95
Prempal Singh <prempal.singh@zomato.com>
2019-03-28 12:02:14 +0530

    Revert "migrate to AndroidX"
    This reverts commit 4de4673c0acda31c3d328fcc27b38d21202122a0.

--- app/src/main/java/com/zomato/sushiapp/fragments/ImageViewsFragment.kt
+++ app/src/main/java/com/zomato/sushiapp/fragments/ImageViewsFragment.kt
@@ -2,7 +2,7 @@ package com.zomato.sushiapp.fragments
 
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
