2c6c703172fc6f8bf7cfd6f5ecb15f69b71e773c
Prempal Singh <prempal.singh@zomato.com>
2019-04-02 11:03:49 +0530

    create button styles

---
+++ sushilib/src/main/java/com/zomato/sushilib/atoms/buttons/SushiButton.kt
@@ -0,0 +1,20 @@
+package com.zomato.sushilib.atoms.buttons
+
+import android.content.Context
+import android.support.design.button.MaterialButton
+import android.util.AttributeSet
+
+class SushiButton : MaterialButton {
+
+    constructor(context: Context) : super(context) {
+
+    }
+
+    constructor(context: Context, attrs: AttributeSet) : super(context, attrs) {
+
+    }
+
+    constructor(context: Context, attrs: AttributeSet, defStyleAttr: Int) : super(context, attrs, defStyleAttr) {
+
+    }
+}
