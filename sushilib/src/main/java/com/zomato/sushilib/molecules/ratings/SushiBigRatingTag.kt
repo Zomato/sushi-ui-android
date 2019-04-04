66b3e1b059088ef825f050bf8ccd74879293a2b2
Prateek Sharma <sharma.prateek@zomato.com>
2019-04-04 18:12:17 +0530

    tag changes

--- sushilib/src/main/java/com/zomato/sushilib/molecules/ratings/SushiBigRatingTag.kt
+++ sushilib/src/main/java/com/zomato/sushilib/molecules/ratings/SushiBigRatingTag.kt
@@ -8,31 +8,21 @@ import com.zomato.sushilib.R
 import com.zomato.sushilib.atoms.tagviews.SushiTagView
 
 
-open class SushiBigRatingTag : SushiTagView,
-    SushiRatingViewSetter {
+open class SushiBigRatingTag @JvmOverloads constructor(
+    context: Context?,
+    attrs: AttributeSet? = null,
+    defStyleAttr: Int = 0,
+    defStyleRes: Int = R.style.SushiTheme_BigRatingTag
+) : SushiTagView(context, attrs, defStyleAttr, defStyleRes), SushiRatingViewSetter {
 
-    private val sushiRatingViewImpl: SushiRatingViewImpl
-
-
-
-    constructor(context: Context?) : this(context, null)
-
-    constructor(context: Context?, attrs: AttributeSet?) : this(
-        context,
-        attrs,
-        0,
-        R.style.SushiTheme_BigRatingTag
-    )
-
-    constructor(context: Context?, attrs: AttributeSet?, defStyleAttr: Int, defStyleRes: Int)
-            : super(context, attrs, defStyleAttr, defStyleRes) {
-        sushiRatingViewImpl = SushiRatingViewImpl(this, attrs, defStyleAttr, defStyleRes)
-    }
-
+    private val sushiRatingViewImpl: SushiRatingViewImpl = SushiRatingViewImpl(this, attrs, defStyleAttr, defStyleRes)
 
     override fun onDraw(canvas: Canvas?) {
         super.onDraw(canvas)
-        Log.e("test_Tag", "draw called width = $width" + " 44dp = ${resources.getDimensionPixelSize(R.dimen.sushi_big_rating_min_width)}")
+        Log.e(
+            "test_Tag",
+            "draw called width = $width" + " 44dp = ${resources.getDimensionPixelSize(R.dimen.sushi_big_rating_min_width)}"
+        )
         sushiRatingViewImpl.dispatchDraw(canvas)
     }
 
