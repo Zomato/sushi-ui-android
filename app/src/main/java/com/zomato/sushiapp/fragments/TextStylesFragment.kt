0134b2db811d77f0271f358c1af5950e23b08e23
Arnav Gupta <arnav.gupta@zomato.com>
2019-04-04 12:45:12 +0530

    typography child view
    Signed-off-by: Arnav Gupta <arnav.gupta@zomato.com>

--- app/src/main/java/com/zomato/sushiapp/fragments/TextStylesFragment.kt
+++ app/src/main/java/com/zomato/sushiapp/fragments/TextStylesFragment.kt
@@ -2,12 +2,15 @@ package com.zomato.sushiapp.fragments
 
 
 import android.os.Bundle
+import android.support.design.widget.TabLayout
 import android.support.v4.app.Fragment
+import android.support.v4.app.FragmentPagerAdapter
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-
 import com.zomato.sushiapp.R
+import com.zomato.sushiapp.fragments.typography.TypographyStyleFragment
+import kotlinx.android.synthetic.main.fragment_text_styles.view.*
 
 /**
  * A simple [Fragment] subclass.
@@ -20,7 +23,19 @@ class TextStylesFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_text_styles, container, false)
+        val rootView = inflater.inflate(R.layout.fragment_text_styles, container, false)
+
+        rootView.viewpager_font_weight.adapter = object : FragmentPagerAdapter(childFragmentManager) {
+            override fun getItem(pos: Int): Fragment =
+                TypographyStyleFragment.newInstance(pos)
+
+            override fun getCount(): Int = 5
+
+            override fun getPageTitle(pos: Int): CharSequence? =
+                TypographyStyleFragment.getTitle(pos)
+
+        }
+        return rootView
     }
 
 
