606140d21faad73202bf60d3bd77d246e8e0b69d
Prempal Singh <prempal.singh@zomato.com>
2019-05-17 11:33:48 +0530

    add rating change listener to SushiRatingBar

--- app/src/main/java/com/zomato/sushiapp/fragments/TagsFragment.kt
+++ app/src/main/java/com/zomato/sushiapp/fragments/TagsFragment.kt
@@ -1,15 +1,23 @@
 package com.zomato.sushiapp.fragments
 
 import android.os.Bundle
-import android.support.annotation.IntDef
 import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import com.zomato.sushiapp.R
+import com.zomato.sushilib.organisms.ratings.SushiRatingBar
 
 class TagsFragment : Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.fragment_tag, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        view.findViewById<SushiRatingBar>(R.id.rating_bar).setOnRatingChangeListener {
+            Toast.makeText(context, it.toString(), Toast.LENGTH_SHORT).show()
+        }
     }
 }
