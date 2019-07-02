98107bdafa737134692df43e98b5885e2309f7c7
Avi <b2aguy@gmail.com>
2019-07-02 13:11:15 +0530

    Updating sushi color palatte

--- sushilib/src/main/java/com/zomato/sushilib/utils/color/ColorBuilder.kt
+++ sushilib/src/main/java/com/zomato/sushilib/utils/color/ColorBuilder.kt
@@ -109,8 +109,18 @@ class ColorBuilder(val context: Context) {
                 900 -> R.color.sushi_purple_900
                 else -> R.color.sushi_purple_500
             }
+            ColorName.LIME -> when (tint) {
+                100 -> R.color.sushi_lime_100
+                200 -> R.color.sushi_lime_200
+                300 -> R.color.sushi_lime_300
+                500 -> R.color.sushi_lime_500
+                700 -> R.color.sushi_lime_700
+                900 -> R.color.sushi_lime_900
+                else -> R.color.sushi_lime_500
+            }
             ColorName.INDIGO -> when (tint) {
                 100 -> R.color.sushi_indigo_100
+                200 -> R.color.sushi_indigo_200
                 300 -> R.color.sushi_indigo_300
                 500 -> R.color.sushi_indigo_500
                 700 -> R.color.sushi_indigo_700
@@ -119,6 +129,7 @@ class ColorBuilder(val context: Context) {
             }
             ColorName.BROWN -> when (tint) {
                 100 -> R.color.sushi_brown_100
+                200 -> R.color.sushi_brown_200
                 300 -> R.color.sushi_brown_300
                 500 -> R.color.sushi_brown_500
                 700 -> R.color.sushi_brown_700
@@ -127,6 +138,7 @@ class ColorBuilder(val context: Context) {
             }
             ColorName.TEAL -> when (tint) {
                 100 -> R.color.sushi_teal_100
+                200 -> R.color.sushi_teal_200
                 300 -> R.color.sushi_teal_300
                 500 -> R.color.sushi_teal_500
                 700 -> R.color.sushi_teal_700
@@ -135,6 +147,7 @@ class ColorBuilder(val context: Context) {
             }
             ColorName.ORANGE -> when (tint) {
                 100 -> R.color.sushi_orange_100
+                200 -> R.color.sushi_orange_200
                 300 -> R.color.sushi_orange_300
                 500 -> R.color.sushi_orange_500
                 700 -> R.color.sushi_orange_700
@@ -143,6 +156,7 @@ class ColorBuilder(val context: Context) {
             }
             ColorName.PINK -> when (tint) {
                 100 -> R.color.sushi_pink_100
+                200 -> R.color.sushi_pink_200
                 300 -> R.color.sushi_pink_300
                 500 -> R.color.sushi_pink_500
                 700 -> R.color.sushi_pink_700
