6bf5b0cd715de19ca556ddac838aa957c6a47c87
Arnav Gupta <arnav.gupta@zomato.com>
2020-01-29 14:41:27 +0530

    update CNAME to android.sushi.design
    Signed-off-by: Arnav Gupta <arnav.gupta@zomato.com>

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -1,7 +1,9 @@
+const sidebar = require('../sidebar');
 module.exports = {
   title: 'Sushi UI Kit for Android',
   description:
     'A delicious UI Kit to build Android apps. Made with ❤ by Zomato',
+  base: '/Sushi-Android/',
   themeConfig: {
     nav: [
       { text: 'Guide', link: '/pages/' },
@@ -9,45 +11,6 @@ module.exports = {
       { text: 'Github', link: 'https://github.com/Zomato/Sushi-Android' }
     ],
     sidebarDepth: 2,
-    sidebar: [
-      '/pages/',
-      {
-        collapsable: false,
-        title: 'Resources',
-        children: [
-          '/pages/01-resources/Typography.md',
-          '/pages/01-resources/Colors.md'
-        ]
-      },
-      {
-        collapsable: false,
-        title: 'Atoms',
-        children: [
-          '/pages/02-atoms/SushiTextView.md',
-          '/pages/02-atoms/SushiButton.md',
-          '/pages/02-atoms/SushiCircleImageView.md',
-          '/pages/02-atoms/SushiRoundedImageView.md'
-        ]
-      },
-      {
-        collapsable: false,
-        title: 'Molecules',
-        children: [
-          'pages/03-molecules/SushiTag.md',
-          'pages/03-molecules/SushiTextInputField.md',
-          'pages/03-molecules/SushiSwitch.md',
-          'pages/03-molecules/SushiRadioButton.md',
-          'pages/03-molecules/SushiCheckBox.md'
-        ]
-      },
-      {
-        collapsable: false,
-        title: 'Organisms',
-        children: [
-          'pages/04-organisms/SushiRatingBar.md',
-          'pages/04-organisms/SushiTabLayout.md'
-        ]
-      }
-    ]
+    sidebar
   }
 };
