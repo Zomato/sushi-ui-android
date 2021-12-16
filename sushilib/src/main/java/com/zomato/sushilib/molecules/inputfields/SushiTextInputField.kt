d587751c882ae10cceec52165bbb080686e82d1b
Jazib <jazib.khan@zomato.com>
2021-12-16 10:11:23 +0530

    Add attr to update editText type

--- sushilib/src/main/java/com/zomato/sushilib/molecules/inputfields/SushiTextInputField.kt
+++ sushilib/src/main/java/com/zomato/sushilib/molecules/inputfields/SushiTextInputField.kt
@@ -10,7 +10,9 @@ import android.util.AttributeSet
 import android.util.LayoutDirection
 import android.util.TypedValue
 import android.view.MotionEvent
+import android.widget.EditText
 import androidx.annotation.AttrRes
+import androidx.annotation.DimenRes
 import androidx.core.content.ContextCompat
 import com.google.android.material.textfield.TextInputEditText
 import com.google.android.material.textfield.TextInputLayout
@@ -59,13 +61,8 @@ open class SushiTextInputField @JvmOverloads constructor(
             if (attrInputType != -1) {
                 editText.inputType = attrInputType
             }
-            val attrEditTextSize = it.getDimension(R.styleable.SushiTextInputField_editTextSize, 0f)
-            if (attrEditTextSize != 0f) {
-                editText.setTextSize(
-                    TypedValue.COMPLEX_UNIT_PX,
-                    attrEditTextSize
-                )
-            }
+            val type = it.getInt(R.styleable.SushiTextInputField_editTextType, -1)
+            setEditTextType(type)
             editText.applyDrawables(
                 attrs, defStyleAttr,
                 ContextCompat.getColor(context, R.color.sushi_grey_400) ?: Color.GRAY,
@@ -75,6 +72,37 @@ open class SushiTextInputField @JvmOverloads constructor(
             it.recycle()
         }
         this.addView(editText)
+    }
+
+    private fun setEditTextType(type: Int) {
+        resolveAndSetType(editText, type)
+    }
+
+    private fun resolveAndSetType(editText: TextInputEditText, type: Int) {
+        if (type == -1) return
+        editText.setTextSize(
+            TypedValue.COMPLEX_UNIT_PX,
+            editText.context.resources.getDimensionPixelOffset(resolveZTextType(type)).toFloat()
+        )
+        val textFontWeight = (type / 10 + 3) * 100
+        this.textFontWeight = textFontWeight
+    }
+
+    @DimenRes
+    private fun resolveZTextType(type: Int): Int {
+        return when (type % 10) {
+            0 -> R.dimen.sushi_textsize_050
+            1 -> R.dimen.sushi_textsize_100
+            2 -> R.dimen.sushi_textsize_200
+            3 -> R.dimen.sushi_textsize_300
+            4 -> R.dimen.sushi_textsize_400
+            5 -> R.dimen.sushi_textsize_500
+            6 -> R.dimen.sushi_textsize_600
+            7 -> R.dimen.sushi_textsize_700
+            8 -> R.dimen.sushi_textsize_800
+            9 -> R.dimen.sushi_textsize_900
+            else -> R.dimen.sushi_textsize_500
+        }
     }
 
     /**
